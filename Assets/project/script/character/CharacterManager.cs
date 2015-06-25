fbae623dbf11890d92893ac6904a30f2f78de05a
Shaula <the9ball@gmail.com>
2015-06-26 07:16:04 +0900

    [CharacterManager] 作成

---
+++ Assets/project/script/character/CharacterManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CharacterManager
+{
+	public List<Character> characterList;
+
+	public void CreateCharacterList(
+		IEnumerable<CharacterData> dataList
+	)
+	{
+		characterList = dataList
+			.Select( x => CharacterFactory.Create( x ) )
+			.ToList();
+	}
+}
+
