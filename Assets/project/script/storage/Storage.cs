89d57f06d4230372fe08a09217913a4b479207b6
Shaula <the9ball@gmail.com>
2015-06-26 07:24:10 +0900

    [Storage] 作成

---
+++ Assets/project/script/storage/Storage.cs
@@ -0,0 +1,10 @@
+﻿public static class Storage
+{
+	public static CharacterManager CharacterManager = new CharacterManager();
+
+	public static void Update()
+	{
+		CharacterManager.Update();
+	}
+}
+
