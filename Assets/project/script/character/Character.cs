14e2895dbae8628c65a90e6c45b9a5e6d41b2ed9
Shaula <the9ball@gmail.com>
2015-06-26 07:05:02 +0900

    [Character] 作成

---
+++ Assets/project/script/character/Character.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IMovable
+{
+	Vector3    position { get; }
+	Quaternion rotation { get; }
+
+	void WarpTo( Vector3 to );
+	void MoveTo( Vector3 to );
+	void Move( Vector3 direction );
+}
+
+public class Character
+{
+	public Vector3    position { get { return moveEngine.position; } }
+	public Quaternion rotation { get { return moveEngine.rotation; } }
+
+	protected IMovable moveEngine;
+
+	public Character( IMovable moveEngine )
+	{
+		this.moveEngine = moveEngine;
+	}
+}
+
