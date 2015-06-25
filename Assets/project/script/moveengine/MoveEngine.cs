b46f4e4d3f851741b19679bdfb50b2a696ec3928
Shaula <the9ball@gmail.com>
2015-06-26 07:04:19 +0900

    [MoveEngine] 作成

---
+++ Assets/project/script/moveengine/MoveEngine.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MoveEngine : MonoBehaviour, IMovable
+{
+	public Vector3    position { get { return this.transform.position; } }
+	public Quaternion rotation { get { return this.transform.rotation; } }
+
+	public void WarpTo( Vector3 to )
+	{
+		transform.position = to;
+	}
+
+	public void MoveTo( Vector3 to )
+	{
+		transform.position = to;
+	}
+
+	public void Move( Vector3 direction )
+	{
+		transform.position += direction;
+	}
+}
