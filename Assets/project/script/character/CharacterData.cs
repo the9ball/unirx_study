2882492229e535a4eb991c6e40a1c114f2d292dd
Shaula <the9ball@gmail.com>
2015-06-26 07:10:10 +0900

    [CharacterData] 追加

---
+++ Assets/project/script/character/CharacterData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MoveEngine))]
+public class CharacterData : MonoBehaviour
+{
+	private IMovable moveEngine;
+	public IMovable MoveEngine
+	{
+		get { return ( moveEngine = ( moveEngine ?? GetComponent<IMovable>() ) ); }
+	}
+}
+
